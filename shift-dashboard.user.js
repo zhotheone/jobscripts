cca6b74d0df7af2884df558b29a971e45111aa44
zhotheone <98022845+zhotheone@users.noreply.github.com>
2025-06-26 13:44:03 +0300

    Update shift-dashboard.user.js

--- shift-dashboard.user.js
+++ shift-dashboard.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Speedy Company Shift Dashboard
+// @name         Shift Dashboard
 // @namespace    https://github.com/zhotheone/jobscripts
 // @version      1.0
 // @description  A better way to handle schedule.
